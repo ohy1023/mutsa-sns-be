87f35f78c87e0269fdf0e0c5328925c5ef038b8d
ohy1023 <zvyg1023@naver.com>
2022-12-20 20:14:16 +0900

    user 회원가입

--- src/main/java/com/likelionsns/final_project/domain/dto/Response.java
+++ src/main/java/com/likelionsns/final_project/domain/dto/Response.java
@@ -3,12 +3,14 @@ package com.likelionsns.final_project.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 public class Response<T> {
     private String resultCode;
     private T result;
+
 
     public static Response<Void> error(String resultCode) {
         return new Response(resultCode, null);
@@ -17,4 +19,6 @@ public class Response<T> {
     public static <T> Response<T> success(T result) {
         return new Response("SUCCESS", result);
     }
+
+
 }
--- src/main/java/com/likelionsns/final_project/service/UserService.java
+++ src/main/java/com/likelionsns/final_project/service/UserService.java
@@ -3,9 +3,13 @@ package com.likelionsns.final_project.service;
 import com.likelionsns.final_project.domain.dto.UserJoinRequest;
 import com.likelionsns.final_project.domain.dto.UserJoinResponse;
 import com.likelionsns.final_project.domain.entity.User;
+import com.likelionsns.final_project.exception.ErrorCode;
+import com.likelionsns.final_project.exception.SnsAppException;
 import com.likelionsns.final_project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.likelionsns.final_project.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +17,10 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserJoinResponse join(UserJoinRequest userJoinRequest) {
+        userRepository.findByUserName(userJoinRequest.getUserName())
+                .ifPresent((user -> {
+                    throw new SnsAppException(DUPLICATED_USER_NAME, String.format("%s는 이미 존재합니다.",user.getUserName()));
+                }));
         User savedUser = userRepository.save(userJoinRequest.toEntity());
         return UserJoinResponse.builder()
                 .userId(savedUser.getId())
