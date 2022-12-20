1ab5285c73f9b7a6bca78f44d18f2245b0c61dfe
ohy1023 <zvyg1023@naver.com>
2022-12-20 15:55:25 +0900

    ci/cd 테스트

--- src/main/java/com/likelionsns/final_project/controller/HelloController.java
+++ src/main/java/com/likelionsns/final_project/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok().body("test");
+        return ResponseEntity.ok().body("hello");
     }
 }
