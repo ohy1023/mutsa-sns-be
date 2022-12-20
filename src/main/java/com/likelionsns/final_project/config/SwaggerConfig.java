8d85e08ad9713375ecaba279968996b5ae888a25
ohy1023 <zvyg1023@naver.com>
2022-12-20 11:23:24 +0900

    swagger config 추가 및 hello 출력 컨트롤러 작성

---
+++ src/main/java/com/likelionsns/final_project/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.likelionsns.final_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.likelionsns.final_project"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
