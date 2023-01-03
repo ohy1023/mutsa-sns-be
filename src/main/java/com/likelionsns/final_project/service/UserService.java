204eec7d2cb687eb1b85f3244e4d3f94c86e5d45
ohy1023 <zvyg1023@naver.com>
2023-01-03 15:27:41 +0900

    user 회원가입/로그인

--- src/main/java/com/likelionsns/final_project/service/UserService.java
+++ src/main/java/com/likelionsns/final_project/service/UserService.java
@@ -4,13 +4,11 @@ import com.likelionsns.final_project.domain.dto.UserDto;
 import com.likelionsns.final_project.domain.request.UserJoinRequest;
 import com.likelionsns.final_project.domain.response.UserJoinResponse;
 import com.likelionsns.final_project.domain.entity.User;
-import com.likelionsns.final_project.exception.ErrorCode;
 import com.likelionsns.final_project.exception.SnsAppException;
 import com.likelionsns.final_project.repository.UserRepository;
 import com.likelionsns.final_project.utils.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -32,7 +30,7 @@ public class UserService {
     public UserJoinResponse join(UserJoinRequest userJoinRequest) {
         userRepository.findByUserName(userJoinRequest.getUserName())
                 .ifPresent((user -> {
-                    throw new SnsAppException(DUPLICATED_USER_NAME, String.format("%s는 이미 존재합니다.",user.getUserName()));
+                    throw new SnsAppException(DUPLICATED_USER_NAME, DUPLICATED_USER_NAME.getMessage());
                 }));
         User savedUser = userRepository.save(userJoinRequest.toEntity(encoder.encode(userJoinRequest.getPassword())));
         return UserJoinResponse.builder()
@@ -45,18 +43,18 @@ public class UserService {
         //userName 있는지 여부 확인
         //없으면 NOT_FOUND 에러 발생
         User user = userRepository.findByUserName(userName)
-                .orElseThrow(() -> new SnsAppException(USERNAME_NOT_FOUND, String.format("%s는 존재하지 않습니다.", userName)));
+                .orElseThrow(() -> new SnsAppException(USERNAME_NOT_FOUND, USERNAME_NOT_FOUND.getMessage()));
 
         //password 일치 하는지 여부 확인
         if(isWrongPassword(password, user))
-            throw new SnsAppException(INVALID_PASSWORD, String.format("id 또는 password 가 틀렸습니다."));
+            throw new SnsAppException(INVALID_PASSWORD, INVALID_PASSWORD.getMessage());
 
         return JwtUtils.createToken(userName, secretKey, expiredTimeMs);
     }
 
     public UserDto getUserByUserName(String userName) {
         User user = userRepository.findByUserName(userName)
-                .orElseThrow(() -> new SnsAppException(USERNAME_NOT_FOUND, userName + "이 없습니다."));
+                .orElseThrow(() -> new SnsAppException(USERNAME_NOT_FOUND, USERNAME_NOT_FOUND.getMessage()));
         return UserDto.builder()
                 .id(user.getId())
                 .userName(user.getUserName())
