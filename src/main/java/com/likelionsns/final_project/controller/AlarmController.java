7964ea2e17504a1ab75f48d0442df72bfd517ac9
ohy1023 <zvyg1023@naver.com>
2023-01-10 15:25:51 +0900

    README 수정

--- src/main/java/com/likelionsns/final_project/controller/AlarmController.java
+++ src/main/java/com/likelionsns/final_project/controller/AlarmController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/users")
+@RequestMapping("api/v1/")
 public class AlarmController {
 
     private final AlarmService alarmService;
